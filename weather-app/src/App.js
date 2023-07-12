65b113a34c8d6628b7b517fda43fe22eab45c2d1
hvsrathore10 <harshvardhan8991@gmail.com>
2023-07-12 21:24:55 +0530

    Full stack app using MERN

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -2,6 +2,24 @@ import React from 'react'
 import Navbar from './components/Navbar'
 
 export default function App() {
+  const update = async () => {
+    const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Seattle';
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': 'ea03cf9805msh3d33a8f3f09aea3p1f20fcjsnc47c41c87672',
+        'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+      }
+    };
+
+    try {
+      const response = await fetch(url, options);
+      const result = await response.text();
+      console.log(result);
+    } catch (error) {
+      console.error(error);
+    }
+  }
   return (
     <div>
       <Navbar />
